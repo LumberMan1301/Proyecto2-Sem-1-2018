1958b75fee53471046c7449fbb31a34b549218a9
Mariano <marianomm@rocketmail.com>
2018-05-11 22:00:38 -0600

    Organizar las clase

---
+++ src/Estructuras/Ordenamiento/RadixSort.java
@@ -0,0 +1,4 @@
+package Estructuras.Ordenamiento;
+
+public class RadixSort {
+}
---
+++ src/Usuario/Usuario.java
@@ -0,0 +1,12 @@
+package Usuario;
+
+public class Usuario {
+    private String nombreDeUsuario;
+    private String nombre;
+    private int edad;
+    private String Contrasena;
+
+    //generos favoritos
+    //amigos
+
+}
