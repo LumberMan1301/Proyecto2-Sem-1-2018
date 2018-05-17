01c6cdc21a5fe408afb1aeca4b6dc1cc9bc85563
Mariano <marianomm@rocketmail.com>
2018-05-17 00:42:43 -0600

    00.42

--- src/Estructuras/Arboles/BTree.java
+++ src/Estructuras/Arboles/BTree.java
@@ -6,73 +6,74 @@ public class BTree<Key extends Comparable<Key>, Value>
   public Node root;
   public int height;
   public int n;
-  
+
   private static final class Node
   {
     public int m;
     public BTree.Entry[] children = new BTree.Entry[4];
-    
+
     public Node(int k) { m = k; }
   }
-  
+
   private static class Entry
   {
     public Comparable key;
     public final Object val;
     public BTree.Node next;
-    
+
     public Entry(Comparable key, Object val, BTree.Node next) {
       this.key = key;
       this.val = val;
       this.next = next;
     }
   }
-  
+
   public BTree()
   {
     root = new Node(0);
   }
-  
+
   public boolean isEmpty()
   {
     return size() == 0;
   }
-  
+
   public int size()
   {
     return n;
   }
-  
+
   public int height()
   {
     return height;
   }
-  
+
 
   public Value getElement(Key key)
   {
     if (key == null) throw new IllegalArgumentException("Insertar Elemento");
     return search(root, key, height);
   }
-  
+  /**
   private Value search(Node x, Key key, int ht) {
     Entry[] children = children;
-    
+
     if (ht == 0) {
       for (int j = 0; j < m; j++) {
         if (eq(key, key)) { return val;
         }
-        
+
       }
     } else {
       for (int j = 0; j < m; j++) {
-        if ((j + 1 == m) || (less(key, 1key)))
+        if ((j + 1 == m) || (less(key, key)))
           return search(next, key, ht - 1);
       }
     }
     return null;
   }
-  
+
+
   public void insert(Key key, Value val)
   {
     if (key == null) throw new IllegalArgumentException("argument key to put() is null");
@@ -86,7 +87,7 @@ public class BTree<Key extends Comparable<Key>, Value>
     root = t;
     height += 1;
   }
-  
+
   private Node insert(Node h, Key key, Value val, int ht)
   {
     Entry t = new Entry(key, val, null);
@@ -108,7 +109,7 @@ public class BTree<Key extends Comparable<Key>, Value>
         }
       }
     }
-    
+
     for (int i = m; i > j; i--)
       children[i] = children[(i - 1)];
     children[j] = t;
@@ -116,7 +117,7 @@ public class BTree<Key extends Comparable<Key>, Value>
     if (m < 4) return null;
     return split(h);
   }
-  
+
   private Node split(Node h) {
     Node t = new Node(2);
     m = 2;
@@ -124,20 +125,20 @@ public class BTree<Key extends Comparable<Key>, Value>
       children[j] = children[(2 + j)];
     return t;
   }
-  
+
   public String getStringToPrint() {
     return toString(root, height, "") + "\n";
   }
-  
+
   private String toString(Node h, int ht, String indent) {
     StringBuilder s = new StringBuilder();
     Entry[] children = children;
-    
+
     if (ht == 0) {
       for (int j = 0; j < m; j++) {
         s.append(indent + key + " " + val + "\n");
       }
-      
+
     } else {
       for (int j = 0; j < m; j++) {
         if (j > 0) s.append(indent + "(" + key + ")\n");
@@ -146,12 +147,12 @@ public class BTree<Key extends Comparable<Key>, Value>
     }
     return s.toString();
   }
-  
+
   private boolean less(Comparable k1, Comparable k2)
   {
     return k1.compareTo(k2) < 0;
   }
-  
+
   private boolean eq(Comparable k1, Comparable k2) {
     return k1.compareTo(k2) == 0;
   }
